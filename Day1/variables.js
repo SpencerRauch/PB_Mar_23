a14eb1334a00de8ff7edeb7c71ff057bc114d2fe
Spencer Rauch <rauchs@gmail.com>
2023-03-06 13:35:02 -0800

    day 1 afternoon lecture

---
+++ Day1/variables.js
@@ -0,0 +1,34 @@
+// Lets talk variables
+
+//variables store and keep track of data, they will have a name
+
+//keyword var lets us know this will be a variable
+// pineapple will be the name of the variable
+var pineapple = "This is a delicious fruit"; //string
+
+//data types
+
+//string -- a collection of characters wrapped in quotes ' '     " "
+
+var string = "This won't 'work with sin'gle quotes" ;
+
+var quote = ' "This is my quote" - Bob'; 
+
+
+// number -- int, float
+
+var num = 7; //integer -- is a whole number
+var float = 7.1;
+
+// boolean -- true or false
+
+var boolean = true;
+var boolean2 = false;
+
+//undefined
+// the value is not defined, maybe not set yet
+//null
+// the value is specifically empty
+
+
+
