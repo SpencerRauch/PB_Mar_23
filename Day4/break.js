ae918dd1827bab452f3c9e6dcf9dbbfe74c9ab9a
Spencer Rauch <rauchs@gmail.com>
2023-03-09 08:23:41 -0800

    morning lecture

---
+++ Day4/break.js
@@ -0,0 +1,13 @@
+// Let's talk break! 
+
+// breaks out of a loop
+
+
+for (var i = 0; ; i++){
+    if (i > 10){
+        break;
+    }
+    console.log(i)
+}
+
+//keyboard interrupt: ctrl + c   ||   cmd + c
